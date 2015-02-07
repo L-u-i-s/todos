a7962cba5e265b9f0a7a5e496bdd2bdeaa660de2
mariano escalera <quarkmarino@gmail.com>
2015-02-07 02:53:26 -0600

    configures blade templating for slim framework

--- public/index.php
+++ public/index.php
@@ -3,12 +3,22 @@
 	require("../vendor/autoload.php");
 
 	# instantiates a new Slim Application
-	$app = new \Slim\Slim();
+	$app = new \Slim\Slim(array(
+		#Adds application settings
+		'view' => new \Slim\Views\Blade(),
+		'templates.path' => '../templates',
+	));
+
+	#setup templates compiled cache
+	$view = $app->view();
+	$view->parserOptions = array(
+		'debug' => true,
+		'cache' => "../html_cache"
+	);
 
 	# defines a route for the GET method
-	$app->get("/", function(){
-		$view = require("index.html");
-		echo $view;
+	$app->get("/", function() use ($app){
+		$app->render('index');
 	});
 
 	# Actually runs the application
