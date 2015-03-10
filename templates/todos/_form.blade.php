675b6ca2e1992f5120e8219dbce1792f8eba08e0
mariano escalera <quarkmarino@gmail.com>
2015-03-09 23:11:03 -0600

    changes some placeholder and button text in the form

--- templates/todos/_form.blade.php
+++ templates/todos/_form.blade.php
@@ -3,8 +3,8 @@
 
 	<div class="form-group">
 		<label for="">New ToDo Item</label>
-		<input type="text" class="form-control" id="todo_task" name="task" placeholder="Input field">
+		<input type="text" class="form-control" id="todo_task" name="task" placeholder="New Task">
 	</div>
 
-	<button type="submit" class="btn btn-primary pull-right">Submit</button>
+	<button type="submit" class="btn btn-primary pull-right">Submit Task</button>
 </form>
